063328dcf792b870f0fa1ebf8400d0072abad285
Lucas Fanha Felix <96728246+ffelixlucas@users.noreply.github.com>
2023-02-12 22:26:41 -0300

    Curso JS

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 12
+console.log(`a velocidade do seu carro é ${vel}km/h`)
+if (vel >60){ //condição simples!
+    console.log(`Você ultapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`dirija sempre usando cinto de segurança!`)
