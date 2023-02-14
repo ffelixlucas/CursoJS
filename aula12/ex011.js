00957489ecde860f2cf0ee961f02ff53acb0ab3d
Lucas Fanha Felix <96728246+ffelixlucas@users.noreply.github.com>
2023-02-13 22:01:04 -0300

    Curso JS

---
+++ aula12/ex011.js
@@ -0,0 +1,10 @@
+var  idade =67
+console.log(`Voce tem ${idade} anos!`)
+if (idade < 16) {
+    console.log('Não vota')
+} else  if (idade < 18 || idade > 65) {
+    console.log('Voto opcional!')    
+}else {
+    console.log('Voto Obrigatorio!!!')
+}
+
