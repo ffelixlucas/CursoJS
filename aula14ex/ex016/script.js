aeccf6dc27066843ef7e4dcf9e5dde5a8548546d
Lucas Fanha Felix <96728246+ffelixlucas@users.noreply.github.com>
2023-02-16 10:40:17 -0300

    Curso JS

---
+++ aula14ex/ex016/script.js
@@ -0,0 +1,33 @@
+function contar(){
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+
+    if(ini.value.length == 0 || fim.value.length == 0 ||  passo.value.length == 0){
+        res.innerHTML = 'Impossivel contar!'
+        //window.alert('[ERROR] Faltam dados!')
+    }else {
+      res.innerHTML = 'Contando:<br>' 
+      let i = Number(ini.value) 
+      let f = Number(fim.value)
+      let p = Number(passo.value)
+      if (p <= 0){
+          window.alert('Passo inválido considerando seu passo 1 !') 
+          p = 1
+      }
+      if (i < f) {
+        // Contagem crescente      
+        for(let c = i; c <=f; c += p){
+            res.innerHTML += ` ${c} \u{1F449}` 
+      }
+        } else {
+            // Contagem regressiva
+            for(let c = i; c >=f; c -= p){
+                res.innerHTML += ` ${c} \u{1F449}` 
+          }
+          
+        }
+res.innerHTML += `\u{1F3C1}`
+}
+}
