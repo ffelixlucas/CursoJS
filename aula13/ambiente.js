fd0a46bb770f65aeb73aa8d3f17c15e630076774
Lucas Fanha Felix <96728246+ffelixlucas@users.noreply.github.com>
2023-02-16 08:38:43 -0300

    Curso JS

---
+++ aula13/ambiente.js
@@ -0,0 +1,20 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+/*var c = 1
+while (c <= 2){
+    console.log(`Passo ${c}`)
+    c++
+}*/
+
+/*
+console.log(`Passo 1`)
+console.log(`Passo 2`)
+console.log(`Passo 3`)
+console.log(`Passo 4`)
+console.log(`Passo 5`)
+console.log(`Passo 6`)
+*/
