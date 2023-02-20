af3187f3f885954be5894a2637b329336c2dbce9
Lucas Fanha Felix <96728246+ffelixlucas@users.noreply.github.com>
2023-02-19 22:10:10 -0300

    CursoJS

--- aula16ex/script.js
+++ aula16ex/script.js
@@ -44,9 +44,37 @@ function maiorValor() {
     }
     return max
 }
+function menorValor(){
+    let min = valores[0]
+    for (let c = 1; c < valores.length; c++){
+        if (valores[c]< min){
+            min = valores[c]
+        }
+    }
+    return min
+}
+function soma(){
+    let total = 0 
+    for (let c = 0; c < valores.length; c++){
+        total +=valores[c]
+    }
+    return total
+}
   
 function finalizar(){
     res.innerHTML = `Ao todo, temos ${valores.length} numeros cadastrados`
     let max = maiorValor()
+    let min = menorValor()
+    let total = soma()
     res.innerHTML += `<br>Maior valor informado foi o ${max}`
+    res.innerHTML += `<br>Menor valor informado foi o ${min}`
+    res.innerHTML += `<br> A soma dos Valores Informado é ${total}`
+    media = total / valores.length
+    res. innerHTML += `<br>A Media dos valores Adicionados é ${media}`
 }
+function limpar() {
+    valores = []; // Limpa o array de valores
+    lista.innerHTML = ''; // Limpa a lista
+    res.innerHTML = ''; // Limpa a div de resultados
+  }
+  
