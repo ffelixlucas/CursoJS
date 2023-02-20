2dff3e5117bc95a39678461efcb40fc504d05e5a
Lucas Fanha Felix <96728246+ffelixlucas@users.noreply.github.com>
2023-02-19 22:54:13 -0300

    Update script.js

--- aula16ex/script.js
+++ aula16ex/script.js
@@ -29,13 +29,15 @@ function adicionar(){
               let item = document.createElement('option')
               item.text = `Valor ${num.value} adicionado.`
               lista.appendChild(item)
-              num.value = ''
+              res.innerHTML = ''
               
 }else{
     window.alert('Valor invalido ou ja encontrado na lista!')
     }
+    num.value = ''
+    num.focus()
 }
-function maiorValor() {
+/*function maiorValor() {
     let max = valores[0]
     for (let c = 1; c < valores.length; c++) {
         if (valores[c] > max) {
@@ -62,6 +64,9 @@ function soma(){
 }
   
 function finalizar(){
+    if (valores.length == 0){
+        window.alert('adicione um valor anstes de finalizar!')
+    }else{
     res.innerHTML = `Ao todo, temos ${valores.length} numeros cadastrados`
     let max = maiorValor()
     let min = menorValor()
@@ -72,9 +77,39 @@ function finalizar(){
     media = total / valores.length
     res. innerHTML += `<br>A Media dos valores Adicionados é ${media}`
 }
-function limpar() {
+}
+ESTA FOI A FORMA QUE EU RESOLVI O PROBLEMA SEM A AJUDA DO PROFESSOR*/
+
+function finalizar(){ //ESSA É A RESOLUÇÃO DO PROFESSOR
+    if (valores.length == 0){
+        window.alert('adicione um valor anstes de finalizar!')
+    }else{
+   let tot = valores.length
+   let maior = valores[0]
+   let menor = valores[0]
+   let soma = 0
+   let media = 0
+   for(let pos in valores){
+    soma += valores[pos]
+    if (valores[pos] > maior)
+        maior = valores[pos]
+    if (valores[pos] < menor)
+        menor = valores[pos]
+   }
+   media = soma / tot
+    res.innerHTML = `Ao todo, temos ${tot} numeros cadastrados`
+    res.innerHTML += `<br>Maior valor informado foi o ${maior}`
+    res.innerHTML += `<br>Menor valor informado foi o ${menor}`
+    res.innerHTML += `<br> A soma dos Valores Informado é ${soma}`
+    res. innerHTML += `<br>A Media dos valores Adicionados é ${media}`
+}
+}
+
+
+    function limpar() {
     valores = []; // Limpa o array de valores
     lista.innerHTML = ''; // Limpa a lista
     res.innerHTML = ''; // Limpa a div de resultados
   }
+
   
